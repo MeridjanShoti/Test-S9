74ba04dfb9d9c5fdb4d43f6ca4a19ff5e254261b
MeridjanShoti <meridjanshoti@gmail.com>
2025-02-07 16:25:51 +0100

    a

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import MyNavBar from "../src/components/MyNavBar.jsx";
 import MyFooter from "./components/MyFooter.jsx";
 import MyMain from "./components/MyMain.jsx";
 import Account from "./components/Account.jsx";
+//commento e decommento le pagine per farle vedere singolarmente perchè non credo di avere il tempo di collegarle
 function App() {
   return (
     <>
