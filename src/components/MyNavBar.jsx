1c3f1d7f43e3a6a4d5bafbd4331b7a915c7ab353
MeridjanShoti <meridjanshoti@gmail.com>
2025-02-07 20:01:21 +0100

    a

--- src/components/MyNavBar.jsx
+++ src/components/MyNavBar.jsx
@@ -9,13 +9,32 @@ import { Dropdown } from "react-bootstrap";
 import propic from "../assets/avatar.png";
 import kidsIcon from "../assets/kids_icon.png";
 class MyNavBar extends Component {
-  state = { searchBarShow: false };
+  state = { searchBarShow: false, searchedItem: "" };
+  handleSearch = (e) => {
+    this.setState({
+      searchedItem: e.target.value,
+    });
+    console.log(this.state.searchedItem);
+  };
+  handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(this.state.searchedItem);
+    this.fetchSearch();
+  };
   showSearch = () => {
     if (this.state.searchBarShow === false) {
       this.setState({ searchBarShow: true });
     } else {
       this.setState({ searchBarShow: false });
     }
+  };
+  fetchSearch = () => {
+    fetch(`http://www.omdbapi.com/?s=${this.state.searchedItem}&apikey=9e9a3139`)
+      .then((resp) => resp.json())
+      .then((data) => {
+        this.setState({ movies: data.Search });
+      })
+      .catch((error) => console.error("ERRORE:", error));
   };
   render() {
     return (
@@ -27,7 +46,7 @@ class MyNavBar extends Component {
           <Navbar.Toggle aria-controls="basic-navbar-nav" className="border border-white" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
-              <Nav.Link className="text-white" href="#home">
+              <Nav.Link className="text-white" href="#home" onClick={() => this.props.changeState("home")}>
                 Home
               </Nav.Link>
               <Nav.Link className="text-white" href="#link">
@@ -43,7 +62,7 @@ class MyNavBar extends Component {
                 My List
               </Nav.Link>
             </Nav>
-            <Form className="d-flex ms-auto">
+            <Form className="d-flex ms-auto" onSubmit={this.handleSubmit}>
               <Search className="text-white align-self-center fs-4 mx-2" onClick={this.showSearch} />
 
               <Row className="me-auto align-items-center">
@@ -53,6 +72,7 @@ class MyNavBar extends Component {
                     placeholder="Search"
                     className={this.state.searchBarShow === true ? "mr-sm-2" : "mr-sm-2 d-none"}
                     id="searchBar"
+                    onChange={this.handleSearch}
                   />
                 </Col>
                 <Col>
